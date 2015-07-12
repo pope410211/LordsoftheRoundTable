7674fbc89ef902932dcf1e53eb2728ddaf44ec1b
Nick Pope <pope410211@gmail.com>
2015-07-12 17:54:07 -0400

    refactoring w/ ngRoute

--- app/scripts/lotrt.js
+++ app/scripts/lotrt.js
@@ -3,29 +3,26 @@
   'use strict';
 
 angular.module('lotrt', ['ngRoute']);
-
-// http://www.funnyant.com/angularjs-ui-router/
-.config(function($routeProvider) {
+  .config(function($routeProvider) {
   $routeProvider.otherwise('/home');
 
-  $routeProvider
-      .when('home', {
+  $routeProvider.when('/home', {
         url: '/home',
           templateUrl: '/views/home.html'
           // controller: 'HomeController'
           // console.log(home);
-      })
-      .when('about', {
+      });
+      $routeProvider.when('/about', {
         url: '/about/kick/butt',
           templateUrl: 'views/about.html'
           // controller: 'AboutController'
-      })
-      .when('blog', {
+      });
+      .when('/blog', {
         url: '/blogwon',
           templateUrl: 'views/blog.html'
       })
 
-      .when('community', {
+      .when('/community', {
         url: 'try/the/koolaid',
           templateUrl: 'views/community.html'
       })

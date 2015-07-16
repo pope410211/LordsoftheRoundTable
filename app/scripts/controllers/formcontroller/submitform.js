cd4ef1b8be402241c07222bea2a8a2acb4948c1f
Nick Pope <pope410211@gmail.com>
2015-07-16 09:19:49 -0400

    updating

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -5,17 +5,17 @@
 
   angular.module('lotrt')
 
-  .controller('SubmitForm', function(){
+  .controller('SubmitForm', function($scope, $stateParams){
 
 var firebaseSub = new Firebase('https://lotrttest.firebaseio.com');
 
-var startGroup = firebaseSub.child('newGroup').child('placeholder').set({
-    date_of_game: " ",
-    name_of_group: " ",
-    location_of_game: " ",
-    type_of_game: " ",
-    description: " "
+var startGroup = firebaseSub.child('newGroup');
 
+startGroup.set( {
+
+  test: {
+    'date_of_game': $scope.date_game
+  }
 });
   console.log(startGroup);
 

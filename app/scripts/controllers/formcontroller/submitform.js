38fe0e93988a75b58df183431ce27858d837dc44
Nick Pope <pope410211@gmail.com>
2015-07-16 14:54:30 -0400

    work

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -2,28 +2,32 @@
 (function(){
   'use strict';
 
-    angular.module('lotrt')
-      .controller('submitForm', function ($scope, $firebaseArray) {
+  angular.module('lotrt')
+
+  .controller('submitForm', function($stateParams, $firebase, $firebaseArray) {
+
+var firebaseSub = new Firebase('https://lotrttest.firebaseio.com');
+
+var self = this;
+
+var selfRef = firebaseSub.child('newGroup');
+
+self.submit = $firebaseArray(firebaseSub);
+
+this.submit = function(){
+  self.submit.$add({
+
+      date: self.date,
+      groupName: self.groupName,
+      location: self.local
+
+  })//self.submit
 
 
 
-      var submitForm = new Firebase('https://lotrttest.firebaseio.com');
-      $scope.list = $firebaseArray(submitForm);
-
-
-      $scope.submit = function() {
-        $scope.list.$add({
-          date: $scope.playDate,
-          groupName: $scope.groupName
-        });
-        $scope.date = '';
-        $scope.groupName = '';
-
-
-
-      };
-      console.log($scope.list);
-    })
+};//end of submit function;
+console.log(self);
+});
 
 
 })();

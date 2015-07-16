8a6f39250c8eccd2b511693f38881ddeb4864ea5
Nick Pope <pope410211@gmail.com>
2015-07-16 16:59:37 -0400

    debugging for deploy - changing deploy to live non test site

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -1,24 +1,23 @@
-/* global angular Firebase */
-(function(){
-  'use strict';
-
-  angular.module('lotrt')
-
-  .controller('SubmitController', function($stateParams, $firebase, $firebaseArray) {
-
-var firebaseSub = new Firebase('https://lotrttest.firebaseio.com');
-
-var self = this;
-var findGroup = firebaseSub.child('newGroup');
-var group = {
-  'date': '',
-  'groupName': '',
-} 
-
-
-
-});
-  console.log('work');
-});
-
-})();
+// /* global angular Firebase */
+// (function(){
+//   'use strict';
+//
+//   angular.module('lotrt')
+//
+//   .controller('SubmitController', function($stateParams, $firebase, $firebaseArray) {
+//
+// // var firebaseSub = new Firebase('https://lotrttest.firebaseio.com');
+// //
+// // var self = this;
+// // var findGroup = firebaseSub.child('newGroup');
+// // var group = {
+// //   'date': '',
+// //   'groupName': ''
+// // };
+//
+//
+//
+// });
+//
+//
+// })();

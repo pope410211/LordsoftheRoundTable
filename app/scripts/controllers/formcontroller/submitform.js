cc6673754d3b847ff882dbb431a2e8366c2d276c
Nick Pope <pope410211@gmail.com>
2015-07-17 09:04:26 -0400

    updating

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -7,14 +7,25 @@
   .controller('SubmitController', function($stateParams, $firebase, $firebaseArray) {
 
 var firebaseSub = new Firebase('https://lotrttest.firebaseio.com');
-   var authData = ref.getAuth();
-   var nailsRef = ref.child(authData.uid);
+   var authData = firebaseSub.getAuth();
+   var groupRef = firebaseSub.child(authData.uid);
    var newGroup = {
      'date': '',
      'groupName': ''
    };
 
-   
+   this.newGroup = $firebaseArray();
+   console.log(newGroup);
+   this.submitNail = function($firebaseArray) {
+     this.newGroup.$add({
+       brand: this.brand,
+       name: this.name,
+       family: this.family,
+       tone: this.tone,
+       finish: this.finish,
+       rating: this.rating
+     });
+
 
    };
 

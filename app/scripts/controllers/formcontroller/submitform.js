348d6b2d57fba0509d7c2a92b999db0eab644abb
Nick Pope <pope410211@gmail.com>
2015-07-18 18:46:35 -0400

    working on input forms

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -2,7 +2,7 @@
 (function() {
   'use strict';
   angular.module('lotrt')
-    .controller('SubmitController', function(FIREBASE_URL, $firebaseArray, $location) {
+    .controller('SubmitController', function(FIREBASE_URL, $firebaseArray) {
       var self = this;
       var firebaseSub = new Firebase(FIREBASE_URL);
       var authData = firebaseSub.getAuth();
@@ -11,13 +11,14 @@
         'groupName': '',
         'date': ''
       };
-      this.newGroup = $firebaseArray(submitGroup);
-      console.log(newGroup);
-      this.submit = function() {
-        this.newGroup.$add({
+      self.newGroup = $firebaseArray(submitGroup);
+
+      self.submitGroup = function() {
+        self.newGroup.$add({
           groupName: self.groupName,
           date: self.date
         })
       };
+        console.log(self.submitGroup);
   });
 })();

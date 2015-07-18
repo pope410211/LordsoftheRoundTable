65a9498d1d5ad750070f42ac43d5ada99fe696fc
Nick Pope <pope410211@gmail.com>
2015-07-18 16:22:14 -0400

    prepping to attemt submits

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -1,15 +1,23 @@
 /* global angular Firebase */
 (function() {
   'use strict';
-
   angular.module('lotrt')
-
-  .controller('SubmitController', function($firebase, $firebaseArray, FIREBASE_URL) {
-
-
-
-
+    .controller('SubmitController', function(FIREBASE_URL, $firebaseArray, $location) {
+      var self = this;
+      var firebaseSub = new Firebase(FIREBASE_URL);
+      var authData = ref.getAuth();
+      var submitGroup = ref.child(authData.uid + "/new/group");
+      var newGroup = {
+        'groupName': '',
+        'date': ''
+      };
+      this.newGroup = $firebaseArray(submitGroup);
+      console.log(newGroup);
+      this.submit = function() {
+        this.nailProduct.$add({
+          groupName: self.groupName,
+          date: self.date
+        })
+      };
   });
-
-
 })();

fbd3692a09edf9c3b1e43fe8a13d203a1c0be582
Nick Pope <pope410211@gmail.com>
2015-07-19 01:16:06 -0400

    moving factory to separate file

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -23,27 +23,6 @@ console.log("YES!", saveGroup);
 });//end .controller
 
 
-      .factory('groups', function() {
-     var groups = [];
-
-     return {
-       // CREATE
-       add: function(group) {
-         contact.created_on = new Date;
-
-         // Record the submission...
-         groups.push(group);
-
-         // TODO: Maybe POST this to Firebase one day?
-       },
-       // READ
-       all: function() {
-         return groups.slice();
-         // TODO: Maybe fetch from Firebase one day?
-       },
-       // UPDATE?
-       // DELETE?
-     };
-   }); // END module(form-demo)
+    
 
 })();

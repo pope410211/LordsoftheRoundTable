b61daa1d1983c1d2fc7f3eed092ca1ac1f3eb5b2
Nick Pope <pope410211@gmail.com>
2015-07-20 13:22:46 -0400

    fill out profile info on behalf of user

--- app/scripts/controllers/profilecontroller/profile.js
+++ app/scripts/controllers/profilecontroller/profile.js
@@ -4,26 +4,27 @@
 'use strict';
 
 angular.module('lotrt')
-.controller('profileController', function($scope, $http){
+.controller('profileController', function(FIREBASE_URL, $scope, $http){
 
-  $http.get('api/facebook/user/nick.json').
+var firebasePro = new Firebase(FIREBASE_URL);
+
+  firebasePro.onAuth(function(authData){
+
+     $http.get('https://graph.facebook.com/v2.4/me?access_token=' + authData.facebook.accessToken + '&fields=id,name,picture{height,width,url},age_range,birthday,location').
       success(function(data, status, headers, config) {
         $scope.profile = data;
-        console.log('success profiles', data, status, headers, config );
-      }).
-      error(function(data, status, headers, config) {
-        // log error
-        console.log('Failed profiles', data, status, headers, config);
-      });
+         // this callback will be called asynchronously
+         // when the response is available
+         console.log('yay', data, status, headers, config);
+       }).
+       error(function(data, status, headers, config) {
+         console.log('boo', data, status, headers, config);
+         // called asynchronously if an error occurs
+         // or server returns response with an error status.
+       });
+  });// token auth
 
-
-
-
-
-
-
-});//end of profileController
-
+});
 
 
 })(window);

82f7f695dfbbe5cb3b20009f8b3498bfb672b5f0
Nick Pope <pope410211@gmail.com>
2015-07-20 15:44:54 -0400

    ng-repeat issues

--- app/scripts/controllers/formcontroller/factory.js
+++ app/scripts/controllers/formcontroller/factory.js
@@ -1,33 +1,33 @@
-// /* global angular */
-// (function(){
-//   'use strict';
-//
-//   angular.module('lotrt')
-//   .factory('groups', function() {
-//  var groups = [];
-//
-//  return {
-//    // CREATE
-//    add: function(group) {
-//      group.createdOn = new Date;
-//
-//      // Record the submission...
-//      groups.push(group);
-//
-//      // TODO: Maybe POST this to Firebase one day?
-//    },
-//    // READ
-//   //  all: function() {
-//   //    return groups.slice();
-//   //    // TODO: Maybe fetch from Firebase one day?
-//   //  }
-//    // UPDATE?
-//    // DELETE?
-//  };
-// }); // END module
+/* global angular */
+(function(){
+  'use strict';
+
+  angular.module('lotrt')
+  .factory('games', function() {
+ var groups = [];
+
+ return {
+   // CREATE
+   add: function(group) {
+     game.createdOn = new Date;
+
+     // Record the submission...
+     games.push(game);
+
+     // TODO: Maybe POST this to Firebase one day?
+   },
+   // READ
+   all: function() {
+     return games.slice();
+   // TODO: Maybe fetch from Firebase one day?
+   }
+   // UPDATE?
+   // DELETE?
+ };
+}); // END module
 
 
 
 
 
-// })();
+})();
--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -1,15 +1,21 @@
-/* global angular */
+/* global angular Firbase */
 (function() {
   'use strict';
   angular.module('lotrt')
-    .controller('SubmitController', function(FIREBASE_URL, $http, $firebaseArray) {
-      console.log('inside the controller')
-      var self = this;
+    .controller('SubmitController', function(FIREBASE_URL, $http, $firebaseArray, games) {
+      console.log('inside the controller');
+
+    var self = this;
+
+      this.game = {};
 
       this.saveGroups = function() {
         console.log('anything');
+        groupForm.games.push(this.game);
+        console.log('work');
+        this.game = {};
       };
-      console.log('yes', self.groups);
+
     }); //end .controller
 
 

6b8f7ccf771a87b26ef07fcbe5cae73771b577e2
Nick Pope <pope410211@gmail.com>
2015-07-20 14:08:34 -0400

    update help from @al-the-x

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -3,37 +3,14 @@
   'use strict';
   angular.module('lotrt')
     .controller('SubmitController', function(FIREBASE_URL, $http, $firebaseArray) {
- var self = this;
- $http.get(FIREBASE_URL + '/group/.json')
- .then(function(response){
-    self.groups = response.data;
-    console.log(self.groups);
-});
+      console.log('inside the controller')
+      var self = this;
 
-            var sub = new Firebase('https://lotrttest.firebaseio.com/group/' + 'groups');
-            this.groups = $firebaseArray(sub);
-
-
-            this.saveGroups = function() {
-              // Do nothing if nothing is submitted or if invalid
-              // if (!$form.$dirty || !$form.$valid) {
-              //
-              //  return;
-
-              self.groups.$add({
-                groupName: self.groupName,
-                date: self.date
-              });
-
-
-              self.groups = {};
-               // Reset the form...
-
-
-
-  };
-  console.log('yes', self.groups);
-});//end .controller
+      this.saveGroups = function() {
+        console.log('anything');
+      };
+      console.log('yes', self.groups);
+    }); //end .controller
 
 
 

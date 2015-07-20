9d8f58884a70a1d96a9fb6c4d14a2b6cb62533ad
Nick Pope <pope410211@gmail.com>
2015-07-20 09:29:46 -0400

    idk what I am doing

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -1,34 +1,34 @@
-// /* global angular */
-// (function() {
-//   'use strict';
-//   angular.module('lotrt')
-//     .controller('SubmitController', function(FIREBASE_URL, groups) {
-//  var self = this;
-//
-//
-//             this.group = [];
-//
-//             /**
-//              * @param {FormController} $form submitted
-//              */
-//             self.saveGroup = function($form) {
-//               // Do nothing is nothing if submitted or if invalid
-//               if (!$form.$dirty || !$form.$valid) {
-//
-//                return;
-//
-//               groups.add(self.group);
-//
-//               self.groups = {};
-//                // Reset the form...
-//
-//
-// }
-//   };
-//   console.log('yes', groups);
-// });//end .controller
-//
-//
-//
-//
-// })();
+/* global angular */
+(function() {
+  'use strict';
+  angular.module('lotrt')
+    .controller('SubmitController', function(FIREBASE_URL, groups) {
+ var self = this;
+
+
+            this.group = [];
+
+            /**
+             * @param {FormController} $form submitted
+             */
+            self.saveGroup = function($form) {
+              // Do nothing is nothing if submitted or if invalid
+              if (!$form.$dirty || !$form.$valid) {
+
+               return;
+
+              groups.add(self.group);
+
+              self.groups = {};
+               // Reset the form...
+
+
+}
+  };
+  console.log('yes', groups);
+});//end .controller
+
+
+
+
+})();

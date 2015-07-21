f42f347bc7a7b725e2c402ea2f4e59e09d9867a6
Nick Pope <pope410211@gmail.com>
2015-07-21 16:03:18 -0400

    redid definitions

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -2,7 +2,7 @@
 (function() {
   'use strict';
   angular.module('lotrt')
-    .controller('SubmitController', function(FIREBASE_URL, $http, restangular) {
+    .controller('SubmitController', function(FIREBASE_URL, $http, Restangular) {
       console.log('inside the controller');
 
 // var firebaseSub = new Firebase(FIREBASE_URL);
@@ -12,26 +12,8 @@
     this.games = [];
 
     self.saveGroups = function() {
-
-      $http.post({method: 'JSONP', data: self.games, FIREBASE_URL}).
-        success(function(data) {
-          self.games({
-             groupName: self.groupName
-    // console.log(self.games);
-           });
-           self.groupName = '';
-           // this callback will be called asynchronously
-           // when the response is available
-           console.log('yay', data);
-         }).
-         error(function(data, status, headers, config) {
-           console.log('boo', data, status, headers, config);
-           // called asynchronously if an error occurs
-           // or server returns response with an error status.
-         });
-
-
-
+        // TODO: Create an Ad(Group) from data in the form...
+        // TODO: Put that Ad(Group) in the list of all Ads(Groups)...
      }; // end self.saveGroups
 
 

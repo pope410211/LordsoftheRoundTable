57dfde8f555f6f4ba89cb63e249ffc93a2f42ddc
Nick Pope <pope410211@gmail.com>
2015-08-16 23:33:58 -0500

    everything but date & time work

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -13,7 +13,7 @@
           // 'date': '',
           // 'time': '',
           'location': '',
-          // 'style': '',
+          'style': '',
           'game': '',
           'state': '',
           'age': '',
@@ -31,7 +31,7 @@
             // date: self.date,
             // time: self.time ,
             location: self.local,
-            // style: self.style,
+            style: self.style,
             game: self.game,
             state: self.state,
             age: self.age,
@@ -41,7 +41,6 @@
 
 
           });
-          newGame = {};
         };
 
 
@@ -51,47 +50,3 @@
 
 
 })();
-// this.save = function(){
-//   if (games && isNewGame){
-//     firebaseSub.child('game').set({
-//             groupName: self.game,
-//
-//           });
-//   };
-// };
-
-
-
-
-// SEE BELOW
-
-// var firebaseSub = new Firebase(FIREBASE_URL);
-//
-//     var self = this;
-//     this.game = {
-//       groupName: '',
-//       date: '',
-//       local: '',
-//       description: ''
-//     };
-//     this.games = [];
-//
-//         // TODO: Create an Ad(Group) from data in the form...
-//         // TODO: Put that Ad(Group) in the list of all Ads(Groups)...
-//
-//      var isNewGame = true;
-//
-// self.saveGroups = function(Restangular){
-//   self.games.push(self.game);
-//
-//   console.log('ARRAY', self.games);
-//   this.save = function(){
-//     Restangular.one(games).post({
-//       groupName: groupName.game
-//     });
-//
-//   };
-//
-//   self.game = {};
-// console.log('DATA', Restangular);
-// };

0b94e4f68198fba3cffe940e7703d26d46d452b0
Nick Pope <pope410211@gmail.com>
2015-08-16 20:14:27 -0500

    updating info

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -5,36 +5,7 @@
     .controller('SubmitController', function(FIREBASE_URL, $http, Restangular) {
       console.log('inside the controller');
 
-var firebaseSub = new Firebase(FIREBASE_URL);
 
-    var self = this;
-    this.game = {
-      groupName: '',
-      date: '',
-      local: '',
-      description: ''
-    };
-    this.games = [];
-
-        // TODO: Create an Ad(Group) from data in the form...
-        // TODO: Put that Ad(Group) in the list of all Ads(Groups)...
-
-     var isNewGame = true;
-
-self.saveGroups = function(Restangular){
-  self.games.push(self.game);
-
-  console.log('ARRAY', self.games);
-  this.save = function(){
-    Restangular.one(games).post({
-      groupName: groupName.game
-    });
-
-  };
-
-  self.game = {};
-console.log('DATA', Restangular);
-};
 
     }); //end .controller
 
@@ -49,4 +20,40 @@ console.log('DATA', Restangular);
 //
 //           });
 //   };
+// };
+
+
+
+
+// SEE BELOW
+
+// var firebaseSub = new Firebase(FIREBASE_URL);
+//
+//     var self = this;
+//     this.game = {
+//       groupName: '',
+//       date: '',
+//       local: '',
+//       description: ''
+//     };
+//     this.games = [];
+//
+//         // TODO: Create an Ad(Group) from data in the form...
+//         // TODO: Put that Ad(Group) in the list of all Ads(Groups)...
+//
+//      var isNewGame = true;
+//
+// self.saveGroups = function(Restangular){
+//   self.games.push(self.game);
+//
+//   console.log('ARRAY', self.games);
+//   this.save = function(){
+//     Restangular.one(games).post({
+//       groupName: groupName.game
+//     });
+//
+//   };
+//
+//   self.game = {};
+// console.log('DATA', Restangular);
 // };

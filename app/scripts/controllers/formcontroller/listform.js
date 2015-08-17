1db7b03f140244bb2aca7d219a8b5dbf41fcc960
Nick Pope <pope410211@gmail.com>
2015-08-17 00:11:39 -0500

    working on pulling data

--- app/scripts/controllers/formcontroller/listform.js
+++ app/scripts/controllers/formcontroller/listform.js
@@ -8,11 +8,14 @@ angular.module('lotrt')
 .controller('ListController', function(FIREBASE_URL, $http){
 
 var self = this;
+// var newID = newRef.name();
 
-  $http.get(FIREBASE_URL + '/group.json').
+
+
+  $http.get(FIREBASE_URL + '/newGroup' + '{{key}}' + '.json').
       success(function(data, status, headers, config) {
         self.group = data;
-        console.log('success groups', data, status, headers, config );
+        console.log('success groups', data);
       }).
       error(function(data, status, headers, config) {
         // log error

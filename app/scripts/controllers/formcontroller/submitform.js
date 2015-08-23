aae44b5fc62af3e6bda90e3867c6f3dcebbad692
Nick Pope <pope410211@gmail.com>
2015-08-23 14:59:39 -0400

    pushing object into array

--- app/scripts/controllers/formcontroller/submitform.js
+++ app/scripts/controllers/formcontroller/submitform.js
@@ -9,35 +9,23 @@
       //    value: new Date(2013, 9, 22)
       //  };
         var firebaseSub = new Firebase(FIREBASE_URL);
+
         var authData = firebaseSub.getAuth();
-        var games = firebaseSub.child('/newGroup');
+        var games = firebaseSub.child('/newGroup' + newGroup);
         var newGroup = [];
-        var newGame =  {
-          'groupName': ''
-          // 'date': '',
-          // 'time': '',
-          // 'location': '',
-          // 'style': '',
-          // 'game': '',
-          // 'state': '',
-          // 'age': '',
-          // 'system': '',
-          // 'description': '',
-          // 'user': '',
-          // 'timeStamp': ''
+        var newGame =  {};
+        var partyGame = newGroup.push(newGame);
 
-        };
-
-var newDate = new Date()
+// var newDate = new Date()
 
 
         this.newGame = $firebaseArray(games);
-        console.log(newGame);
+        console.log(partyGame);
         this.saveGroups = function() {
-          this.newGame.$add({
+          this.partyGame.$add({
             user: authData.uid,
             // date: self.date,
-            state: self.state,
+            // state: self.state,
             timeStamp: Firebase.ServerValue.TIMESTAMP,
             groupInfo: {
             groupName: self.groupName
@@ -53,7 +41,8 @@ var newDate = new Date()
 
 
           });
-        };
+          newGame = {};
+        }
 
 
     }); //end .controller

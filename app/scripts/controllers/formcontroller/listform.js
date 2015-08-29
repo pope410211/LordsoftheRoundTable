12fb1fb641dcdc7d8828f37b13f90926d2712e51
Nick Pope <pope410211@gmail.com>
2015-08-29 18:35:06 -0500

    creating new list JS

--- app/scripts/controllers/formcontroller/listform.js
+++ app/scripts/controllers/formcontroller/listform.js
@@ -5,24 +5,20 @@
 'use strict';
 
 angular.module('lotrt')
-.controller('ListController', function(FIREBASE_URL, $http){
+.controller('ListController', function(FIREBASE_URL,  $firebaseArray){
 
 var self = this;
-// var firebaseGroup = new firebaseGroup(FIREBASE_URL + '/newGroup');
+ this.list = [ ];
+ var gameList = new Firebase(FIREBASE_URL + '/newGroup')
+ this.list = $firebaseArray(gameList);
+ console.log(self.list);
 
-// var newID = newRef.name();
 
 
 
-$http.get(FIREBASE_URL + '/newGroup' + '.json').
-    success(function(data) {
-      self.newGroup = data;
-      console.log('success groups', data);
-    }).
-    error(function(data) {
-      // log error
-      console.log('Failed groups', data);
-    });
+
+
+
 
 
 

a28b9fdbe24e71b083bab9c9d71d26d8a919a2c9
Nick Pope <pope410211@gmail.com>
2015-08-29 14:06:35 -0500

    cleaned up fb controller

--- app/scripts/controllers/logincontroller/fblogincontroller.js
+++ app/scripts/controllers/logincontroller/fblogincontroller.js
@@ -58,22 +58,23 @@ function getName(authData) {
    }); //end newUser Auth
 
 
-firebaseFB.onAuth(function(authData){
-
-
-
-   $http.get('https://graph.facebook.com/v2.4/me?access_token=' + authData.facebook.accessToken + '&fields=id,name,picture{height,width,url},age_range,birthday,location').
-    success(function(data, status, headers, config) {
-       // this callback will be called asynchronously
-       // when the response is available
-       console.log('yay', data, status, headers, config);
-     }).
-     error(function(data, status, headers, config) {
-       console.log('boo', data, status, headers, config);
-       // called asynchronously if an error occurs
-       // or server returns response with an error status.
-     });
-});// token auth
+// firebaseFB.onAuth(function(authData){
+//
+//
+//
+//    $http.get('https://graph.facebook.com/v2.4/me?access_token=' + authData.facebook.accessToken + '&fields=id,name,picture,age_range,birthday,location').
+//     success(function(data, status, headers, config) {
+//        // this callback will be called asynchronously
+//        // when the response is available
+//        console.log('yay', data, status, headers, config);
+//      }).
+//      error(function(data, status, headers, config) {
+//        console.log('boo', data, status, headers, config);
+//        // called asynchronously if an error occurs
+//        // or server returns response with an error status.
+//      });
+// });
+// token auth
 
 }); //end .controller('LoginController')
 

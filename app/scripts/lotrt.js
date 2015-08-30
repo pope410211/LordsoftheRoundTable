066b69303d8b9abe0e5a849b81dcedc840fcfcb0
Nick Pope <pope410211@gmail.com>
2015-08-29 20:27:10 -0500

    working on it

--- app/scripts/lotrt.js
+++ app/scripts/lotrt.js
@@ -21,7 +21,7 @@ var lotrt = angular.module('lotrt', ['ui.router', 'restangular', 'firebase', 'an
         controllerAs: 'fblogin'
       })
       .state('myprofile', {
-        url: '/user/myprofile',
+        url: '/user/myprofile/:userID',
         templateUrl: 'views/user/profiles/profile.html',
         controller: 'profileController',
         controllerAs: 'profile'

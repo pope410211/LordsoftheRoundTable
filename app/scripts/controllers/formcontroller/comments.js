4fc74dab5a4156c6473fb789bbedcc9be524a153
Nick Pope <pope410211@gmail.com>
2015-08-30 16:38:44 -0500

    can delete ALL comments....

--- app/scripts/controllers/formcontroller/comments.js
+++ app/scripts/controllers/formcontroller/comments.js
@@ -2,16 +2,18 @@
   'use strict';
 
   angular.module('lotrt')
-  .controller('CommentCtrl', function($stateParams, Restangular, FIREBASE_URL, $http){
+  .controller('CommentCtrl', function($stateParams, Restangular, FIREBASE_URL, $firebaseObject){
 
     console.log('in the comment control center');
     var group = Restangular.one('newGroup', $stateParams.gameID).all('comments');
     var self = this;
     var firebaseAuth = new Firebase(FIREBASE_URL);
     var authData = firebaseAuth.getAuth();
+    var timestamp = new Date().getTime();
 
     this.comments = {
-      'name': authData.facebook.displayName
+      'name': authData.facebook.displayName,
+      'timestamp': timestamp
     };
 
     this.submitComments = function(){
@@ -23,32 +25,19 @@
     };
 
 
+    this.commentList = [ ];
+    var gameList = new Firebase(FIREBASE_URL + '/newGroup/' + $stateParams.gameID + '/comments');
+    this.commentList = $firebaseObject(gameList);
+    console.log(self.commentList);
 
 
-    // group.getList().then(function(comment){
-    // self.comments = comment.plain();
-    // console.log(self.comments);
-    // });
+      this.deleteComment = function(){
 
+          Restangular.one('newGroup', $stateParams.gameID).one('comments/').remove().then(function(){
+                console.log('deleted');
 
-    //  this.commentList = [];
-    //  var commentorList = new Firebase(FIREBASE_URL + '/newGroup/' + $stateParams.gameID + '/comments');
-    //  this.commentList = $firebaseArray(commentorList);
-    //  console.log('yay', self.commentList);
-
-    $http.get(FIREBASE_URL + '/newGroup/' + $stateParams.gameID + '/comments' + '.json' ).
-     success(function(data, status, headers, config) {
-       self.commentList = data;
-
-        console.log('yay', data, status, headers, config);
-      }).
-      error(function(data, status, headers, config) {
-        console.log('boo', data, status, headers, config);
-
-      });
-
-
-
+            });
+};
   });
 //end controller
 
